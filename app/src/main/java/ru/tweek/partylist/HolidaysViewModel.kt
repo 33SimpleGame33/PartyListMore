a3c07939b0b38cd669f799cc590a764b83fc57b9
33SimpleGame33 <yaroslav1999korsakov@mail.ru>
2023-12-21 15:15:42 +0300

    Initial commit

---
+++ app/src/main/java/ru/tweek/partylist/HolidaysViewModel.kt
@@ -0,0 +1,10 @@
+package ru.tweek.partylist
+
+import androidx.lifecycle.ViewModel
+
+class HolidaysViewModel : ViewModel() {
+    var holidaysList: List<Holiday> = emptyList()
+    var showDate: Boolean = true
+    var showdescription: Boolean = true
+
+}
